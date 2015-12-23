69af66d1b1829ec1622f30304bf3b0360126bf9b
Stanols <km-pavlo@mail.ru>
2015-12-23 17:16:07 +0300

    Added Sample project.

---
+++ Sample/Sample.Application.WebApi/Sample.Application.WebApi/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Sample.Application.WebApi
+{
+    public class Server : IDisposable
+    {
+        private IDisposable _server = null;
+        private string _url = "";
+
+        public Server(string url)
+        {
+            _url = url;
+        }
+
+        public void Start()
+        {
+            _server = WebApp.Start<Startup>(url: _url);
+        }
+
+        public void Dispose()
+        {
+            if (_server != null)
+            {
+                _server.Dispose();
+            }
+        }
+    }
+}
