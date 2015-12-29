1735a0e9ac5c16a5e988ecf508d359a2bd8ad826
Stanols <km-pavlo@mail.ru>
2015-12-29 15:41:28 +0300

    Added controllers untracked files.

---
+++ Sample/Sample.Application.WebApi/Sample.Application.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Sample.Application.WebApi.Controllers
+{
+    public class BaseController : ApiController
+    {
+    }
+}
---
+++ Sample/Sample.Application.WebApi/Sample.Application.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Web.Http.Results;
+
+namespace Sample.Application.WebApi.Controllers
+{
+    public class HomeController : BaseController
+    {
+        // GET: api/Home
+        public object Get()
+        {
+            Employee empl = new Employee("Pasha", 22);
+            string hello = empl.SayHello();
+
+            return empl;
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Employee(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string SayHello()
+        {
+            return "Hello, my name is " + this.Name + ". ";
+        }
+    }
+}
